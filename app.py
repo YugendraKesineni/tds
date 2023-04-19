01128abf249475489f6112d1d488d376989ca818
YugendraKesineni <76766577+YugendraKesineni@users.noreply.github.com>
2023-04-19 22:43:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 st.write("""This app gives largest number from given three input numbers""")
-st.header('User Input Parameters)
+st.header('User Input Parameters')
 def user_input_features():
     first_number = st.number_input("Enter the first number",step=1)
     second_number = st.number_input("Enter the second number",step=1)
